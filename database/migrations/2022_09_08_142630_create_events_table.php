f02936bf483f85eb60ac93f3286d152168d62ddf
abematsu keisuke <sakura-jima@live.jp>
2022-09-09 04:50:54 +0900

    dbenum2?

--- database/migrations/2022_09_08_142630_create_events_table.php
+++ database/migrations/2022_09_08_142630_create_events_table.php
@@ -14,18 +14,24 @@ class CreateEventsTable extends Migration
     public function up()
     {
         Schema::create('events', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->unsignedBigIncrements('id',true);
             $table->string('title');
+            $table->unsignedBigIncrements('school_id');
             $table->datetime('event_day');
             $table->string('area');
             $table->integer('target_min_age');
             $table->integer('target_max_age');
+            $table->longText('content');
+            $table->longText('constnt_summary');
             $table->integer('price');
             $table->boolean('price_free');
             $table->boolean('my_event');
             $table->string('event_url');
-            $table->enum('status',[1,2,3]);
-            $table->timestamps();
+            $table->enum('status',['wait','open','end']);
+            $table->softDeletes();
+            $table->timestamps('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamps('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->foreign('school_id')->references('id')->on('users');
 
 
         });
