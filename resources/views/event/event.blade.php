da47e6dad063f1edebd0226e0f0fa2d5d42004e2
abematsu keisuke <sakura-jima@live.jp>
2022-09-15 10:20:05 +0900

    本番環境に向けてトピックの部分にあった無駄なボタン消しましたよ。

--- resources/views/event/event.blade.php
+++ resources/views/event/event.blade.php
@@ -57,7 +57,7 @@
             <div class="carousel-caption d-none d-md-block">
                 <h5>Second slide label</h5>
                 <p>Some representative placeholder content for the second slide.</p>
-                
+
             </div>
             </div>
             <div class="carousel-item">
@@ -68,14 +68,7 @@
             </div>
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
+        
         </div>
 @endsection
 
