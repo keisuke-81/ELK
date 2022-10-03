75439844a7c8a0a751507ff815c36406188d9b0f
abematsu keisuke <sakura-jima@live.jp>
2022-10-03 16:15:51 +0900

    stripeアドレス修正

--- resources/views/event/thanks_form.blade.php
+++ resources/views/event/thanks_form.blade.php
@@ -12,7 +12,7 @@
 <h2>お申込みありがとうございます。</h2>
 <p>決済完了後に登録完了とさせていただいきます。</p>
 {{-- {{ dd($event_images->calendar_url) }} --}}
-<button type="button"   class="btn btn-warning btn-lg" onClick="location.href='{{ ($event_images->calendar_url) }}'">決済画面へ</button>
+<button type="button"   class="btn btn-warning btn-lg" onClick="location.href='{{ ($event_images->outlookcalendar_url) }}'">決済画面へ</button>
 <div></div>
 </div>
 @endsection
