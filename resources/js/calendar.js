118bde6dc687154329ed5483cf6a163586a156da
abematsu keisuke <sakura-jima@live.jp>
2022-10-03 09:14:42 +0900

    カレンダー2022/10修正

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -21,28 +21,146 @@ let calendar = new Calendar(calendarEl, {
     {
       events: [ // put the array in the `events` property
         {
-          title  : '1',
-          start  : '2023-01-01'
+          title  : '9',
+          start  : '2022-10-01'
         },
         {
           title  : '2',
-          start  : '2023-01-05'
-
-        },
-        {
-          title  : '3',
-          start  : '2023-01-09T12:30:00',
-        },
-        {
-          title  : '5',
-          start  : '2022-09-18'
+          start  : '2022-10-02'
 
             },
         {
           title  : '1',
-          start  : '2022-09-25'
+          start  : '2022-10-03'
+        },
+        {
+          title  : '',
+          start  : '2022-10-04'
+
+            },
+        {
+          title  : '1',
+          start  : '2022-10-05'
+        },
+        {
+          title  : '1',
+          start  : '2022-10-06'
+
+            },
+        {
+          title  : '',
+          start  : '2022-10-07'
+        },
+        {
+          title  : '1',
+          start  : '2022-10-08'
+
+            },
+        {
+          title  : '',
+          start  : '2022-10-09'
+        },
+        {
+          title  : '1',
+          start  : '2022-10-10'
+
+            },
+        {
+          title  : '',
+          start  : '2022-10-11'
+        },
+        {
+          title  : '',
+          start  : '2022-10-12'
+
+            },
+        {
+          title  : '',
+          start  : '2022-10-13'
+        },
+        {
+          title  : '',
+          start  : '2022-10-14'
+
+            },
+        {
+          title  : '',
+          start  : '2022-10-15'
+        },
+        {
+          title  : '1',
+          start  : '2022-10-16'
+
+            },
+        {
+          title  : '',
+          start  : '2022-10-17'
+        },
+        {
+          title  : '',
+          start  : '2022-10-18'
+
+            },
+        {
+          title  : '',
+          start  : '2022-10-19'
+        },
+        {
+          title  : '1',
+          start  : '2022-10-20'
+
+            },
+        {
+          title  : '',
+          start  : '2022-10-21'
+        },
+        {
+          title  : '2',
+          start  : '2022-10-22'
+
+            },
+        {
+          title  : '2',
+          start  : '2022-10-23'
+        },
+        {
+          title  : '',
+          start  : '2022-10-24'
+
+            },
+        {
+          title  : '',
+          start  : '2022-10-25'
+        },
+        {
+          title  : '',
+          start  : '2022-10-26'
+
+            },
+        {
+          title  : '',
+          start  : '2022-10-27'
+        },
+        {
+          title  : '1',
+          start  : '2022-10-28'
 
         },
+        {
+          title  : '',
+          start  : '2022-10-29'
+        },
+        {
+          title  : '2',
+          start  : '2022-10-30'
+
+            },
+        {
+          title  : '',
+          start  : '2022-10-31'
+        },
+
+
       ],
       color: 'blue',     // an option!
       textColor: 'yellow' // an option!
